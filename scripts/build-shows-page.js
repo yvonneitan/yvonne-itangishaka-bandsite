c8cda68cba38419370beb071c8dcda5cf085a798
yvonneitan <itan3040@mylaurier.ca>
2024-09-08 21:06:44 -0700

    track files untracked

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,49 @@
+const shows = [
+    {
+      date: 'Mon Sept 09 2024',
+      venue: 'Ronald Lane',
+      location: 'San Francisco, CA',
+    },
+    {
+      date: 'Tue Sept 17 2024',
+      venue: 'Pier 3 East',
+      location: 'San Francisco, CA',
+    },
+    {
+      date: 'Sat Oct 12 2024',
+      venue: 'View Lounge',
+      location: 'San Francisco, CA',
+    },
+    {
+      date: 'Sat Nov 16 2024',
+      venue: 'Hyatt Agency',
+      location: 'San Francisco, CA',
+    },
+    {
+      date: 'Fri Nov 29 2024',
+      venue: 'Moscow Center',
+      location: 'San Francisco, CA',
+    },
+    {
+      date: 'Wed Dec 18 2024',
+      venue: 'Press Club',
+      location: 'San Francisco, CA',
+    }
+  ];
+
+//   <section class="shows">
+//             <h2 class="shows__title">Shows</h2>
+//             <div class="shows__list">
+//                 <div class="shows__item">
+//                     <!-- <h2 class="shows__category">DATE</h2>
+//                     <h3 class="shows__date">Mon Sept 09 2024</h3>
+
+//                     <h2 class="shows__category">VENUE</h2>
+//                     <p class="shows__venue">Ronald Lane </p>
+//                     <h2 class="shows__category">LOCATION</h2>
+//                     <p class="shows__location">San Francisco, CA</p>
+//                     <button class="shows__button">BUY TICKETS</button> -->
+//                 </div>
+//             </div>
+//         </section>
+  
