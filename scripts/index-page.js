580d4dc368e87830140fafac056dd2342c72d72c
yvonneitan <itan3040@mylaurier.ca>
2024-09-14 04:50:19 -0700

    Add reader comment function

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -11,48 +11,55 @@ function parseDate(timestamp) {
   return new Date(Number(timestamp));
 }
 
+function renderComment(comment){
+
+  const detailsEl = document.createElement("li");
+  detailsEl.classList.add("comments__details", "comments__details--border");
+
+  const iconEl = document.createElement("div");
+  iconEl.classList.add("comments__icon");
+
+  const commentsdivEl = document.createElement("div");
+  commentsdivEl.classList.add("comments__container");
+
+  const headerEl = document.createElement("div");
+  headerEl.classList.add("comments__header");
+
+  const authorEl = document.createElement("p");
+  authorEl.classList.add("comments__name");
+  authorEl.innerText = comment.name;
+
+  const dateEl = document.createElement("p");
+  dateEl.classList.add("comments__date");
+  dateEl.innerText = parseDate(
+    comment.timestamp
+  ).toLocaleDateString("en-US");
+
+  headerEl.appendChild(authorEl);
+  headerEl.appendChild(dateEl);
+
+  commentsdivEl.appendChild(headerEl);
+
+  const commentEl = document.createElement("p");
+  commentEl.classList.add("comments__comment");
+  commentEl.innerText = comment.comment;
+  commentsdivEl.appendChild(commentEl);
+
+  detailsEl.appendChild(iconEl);
+  detailsEl.appendChild(commentsdivEl);
+
+  listEl.appendChild(detailsEl);
+
+}
+
 async function displayComments() {
   listEl.innerText = "";
   try {
     const commentsArray = await api.getComments();
 
     for (let i = 0; i < commentsArray.length; i++) {
-      const detailsEl = document.createElement("li");
-      detailsEl.classList.add("comments__details", "comments__details--border");
-
-      const iconEl = document.createElement("div");
-      iconEl.classList.add("comments__icon");
-
-      const commentsdivEl = document.createElement("div");
-      commentsdivEl.classList.add("comments__container");
-
-      const headerEl = document.createElement("div");
-      headerEl.classList.add("comments__header");
-
-      const authorEl = document.createElement("p");
-      authorEl.classList.add("comments__name");
-      authorEl.innerText = commentsArray[i].name;
-
-      const dateEl = document.createElement("p");
-      dateEl.classList.add("comments__date");
-      dateEl.innerText = parseDate(
-        commentsArray[i].timestamp
-      ).toLocaleDateString("en-US");
-
-      headerEl.appendChild(authorEl);
-      headerEl.appendChild(dateEl);
-
-      commentsdivEl.appendChild(headerEl);
-
-      const commentEl = document.createElement("p");
-      commentEl.classList.add("comments__comment");
-      commentEl.innerText = commentsArray[i].comment;
-      commentsdivEl.appendChild(commentEl);
-
-      detailsEl.appendChild(iconEl);
-      detailsEl.appendChild(commentsdivEl);
-
-      listEl.appendChild(detailsEl);
+      renderComment(commentsArray[i]);
+    
     }
   } catch (error) {
     console.error("Error displaying comments", error);
@@ -60,6 +67,8 @@ async function displayComments() {
 }
 
 displayComments();
+
+
 
 const formEl = document.querySelector("#comments-form");
 
